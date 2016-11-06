424efc1c66239def92d1fc814c2de3e014ece63b
Timo Foerster <tfoerster@webfoersterei.de>
2016-11-07 00:30:28 +0100

    Add service-method for contactDetails

--- src/Service/ContactService.php
+++ src/Service/ContactService.php
@@ -36,6 +36,8 @@ class ContactService
     }
 
     /**
+     * Creates a new contact.
+     *
      * @param Contact $contact
      * @return Contact
      * @throws \Exception
@@ -61,6 +63,8 @@ class ContactService
     }
 
     /**
+     * Gets a list of all clients
+     *
      * @return array
      * @throws AuthenticationException
      */
@@ -69,6 +73,58 @@ class ContactService
         $this->authenticationClient->authenticatePartner();
 
         return $this->contactClient->getList();
+    }
+
+    /**
+     * Gets a single contact by handle.
+     *
+     * @param $handle
+     * @return Contact
+     */
+    public function get($handle)
+    {
+        $this->authenticationClient->authenticatePartner();
+
+        $result = $this->contactClient->get($handle);
+
+        $contact = new Contact();
+        $contact->setHandle($handle);
+
+        $contact->setCompany($result['company']);
+        $contact->setFirstname($result['firstname']);
+        $contact->setLastname($result['lastname']);
+
+        list($street, $houseNumber) = $this->convertAddress($result['address']);
+        $contact->setStreet($street);
+        $contact->setHouseNumber($houseNumber);
+
+
+        $contact->setZipCode($result['pcode']);
+        $contact->setCity($result['city']);
+        $contact->setState($result['state']);
+        $contact->setBirthday(null);
+        $contact->setCountry($result['country']);
+        $contact->setPhone($result['telefon']);
+        $contact->setFax($result['fax']);
+        $contact->setMail($result['email']);
+
+        return $contact;
+    }
+
+    /**
+     * Converts a address-line to street and houseNumber
+     *
+     * @param $address
+     * @return array
+     */
+    private function convertAddress($address)
+    {
+        preg_match('/([^\d]+)\s(.+)/i', $address, $result);
+
+        $street = trim($result[1]) ?: null;
+        $houseNumber = trim($result[2]) ?: null;
+
+        return [$street, $houseNumber];
     }
 
 }
