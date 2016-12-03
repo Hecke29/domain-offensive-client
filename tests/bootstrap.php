add5698a49f3646e9b05102996c780128978de7a
Timo Foerster <tfoerster@webfoersterei.de>
2016-12-03 17:26:50 +0100

    Add unit test for entity constraint validation

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,5 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+$loader = require __DIR__ . '/../vendor/autoload.php';
+
+\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader([$loader, 'loadClass']);
